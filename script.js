91f0491d013b4776e0540d2a6cca2ce107a622c6
ZeroDevs <usamau32o@gmail.com>
2025-03-10 09:17:04 +0200

    footer

--- script.js
+++ script.js
@@ -123,6 +123,20 @@ function loadDynamicFooter() {
             </div>
         </div>
         <div class="footer-bottom">
+            <div class="footer-banks">
+                <div class="bank-logo" title="بنك الأهلي">
+                    <img src="images/banks/alahli.png" alt="بنك الأهلي" />
+                </div>
+                <div class="bank-logo" title="بنك الانماء">
+                    <img src="images/banks/alinma.png" alt="بنك الانماء" />
+                </div>
+                <div class="bank-logo" title="بنك الراجحي">
+                    <img src="images/banks/alrajhi.png" alt="بنك الراجحي" />
+                </div>
+                <div class="bank-logo" title="بنك الرياض">
+                    <img src="images/banks/riyad.png" alt="بنك الرياض" />
+                </div>
+            </div>
             <p>© 2024 سوق الأميال السعودي. جميع الحقوق محفوظة.</p>
         </div>
     `;
